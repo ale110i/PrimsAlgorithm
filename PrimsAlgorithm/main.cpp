e3b6f4cd5eebab2596759ff1986a6c56e8c70a2a
Alexey Ischenko <ishhenko.as@phystech.edu>
2020-12-11 03:21:04 +0300

    Initial Commit

---
+++ PrimsAlgorithm/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  PrimsAlgorithm
+//
+//  Created by Alexey Ischenko on 11.12.2020.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
